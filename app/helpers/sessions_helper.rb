2023c9f6b58bccb0413fd783d1f8c0f405bfbe33
nguyenvandatb <nguyen.van.datb@framgia.com>
2017-02-02 11:11:00 +0700

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -9,6 +9,10 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
   def forget(user)
     user.forget
     cookies.delete(:user_id)
@@ -40,5 +44,14 @@ module SessionsHelper
   def destroy
     log_out if logged_in?
     redirect_to root_url
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
